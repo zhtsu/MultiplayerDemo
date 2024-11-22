0e77133df884939e8b02fa976bd5e5f679a047c4
zhtsu <zhtsu@qq.com>
2024-11-22 16:53:22 +0800

    Add DeliveryCounter

---
+++ Assets/Scripts/Kitchen/Counter/DeliveryCounter.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+}
