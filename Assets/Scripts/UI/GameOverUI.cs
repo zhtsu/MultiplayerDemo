705af211228c0873c54b33a9494b452602071ec4
zhtsu <zhtsu@qq.com>
2024-11-27 01:11:17 +0800

    GameOver UI completed

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -4,5 +4,34 @@ using UnityEngine;
 public class GameOverUI : MonoBehaviour
 {
     [SerializeField]
-    private TextMeshProUGUI
+    private TextMeshProUGUI _recipesDeliveredText;
+
+    private void Start()
+    {
+        GameManager.Instance.OnStateChanged += GameManager_OnStateChanged;
+        Hide();
+    }
+
+    private void GameManager_OnStateChanged(object sender, System.EventArgs e)
+    {
+        if (GameManager.Instance.IsGameOver())
+        {
+            Show();
+            _recipesDeliveredText.text = DeliveryManager.Instance.GetSuccessfulRecipesAmount().ToString();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
 }
